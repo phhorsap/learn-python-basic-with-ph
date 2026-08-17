1619cfea6c2bfc606159864f41b2a715af76a22d
phhorsap <43028614+phhorsap@users.noreply.github.com>
2026-08-17 11:03:41 +0000

    Implement basic calculator functionality in Streamlit app

--- 2_Streamlit/app.py
+++ 2_Streamlit/app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-st.title("Hello Streamlit")
-st.write("สวัสดี Streamlit")
+st.title("เครื่องคิดเลข")
+
+a = st.number_input("เลขที่ 1")
+b = st.number_input("เลขที่ 2")
+
+if st.button("คำนวณ"):
+    st.success(f"ผลรวม = {a+b}")
